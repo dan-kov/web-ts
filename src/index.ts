a147f40dc138e68362963317ad7375aea538ed04
dan-kov <dkovalechyn@yahoo.com.br>
2022-06-16 10:48:01 -0300

    first User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Olá');
+import { User } from "./models/User";
+
+
+
+const user = new User({
+    name: 'MyName',
+    age: 22
+});
+
+user.set({age: 44, name: 'Teste2'});
+user.set({age: 54});
+console.log(user);
+
