5bb1d97695e9c1d3ebcf2ee39394026493b27eab
dan-kov <dkovalechyn@yahoo.com.br>
2022-06-16 19:54:42 -0300

    Sync, attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,46 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps{
+export interface UserProps{
     id?: number;
     name?: string;
     age?: number;
 }
 
-
+const rootUrl = 'http://localhost:1234/users';
+/*
+.then( (response: AxiosResponse): void => {
+    this.set(response.data);
+})
+*/
 
 export class User{
     public events: Eventing = new Eventing();
+    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-    constructor(private data: UserProps){}
-
-    get(propName: string): (string|number){
-        return this.data[propName];
-    }
-
-    set(update: UserProps): void{
-        Object.assign(this.data, update);
-    }
-
-    fetch():void{
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-            .then( (response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    save(): void{
-        const id =  this.data.id;
-        if ( id ){
-            //put
-            axios.put(`http://localhost:3000/users/${id}`, this.data);
-        }
-        else{
-            axios.post(`http://localhost:3000/users`, this.data);
-        }
-
-    }
-
-
-
+  
 }
