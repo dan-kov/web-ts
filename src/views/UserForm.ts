8541645ba2b5523acfbaf8f4ada053e0d2ffc24c
dan-kov <dkovalechyn@yahoo.com.br>
2022-06-19 08:48:15 -0300

    event binding

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,13 +3,45 @@
 export class UserForm{
     constructor(public parent: HTMLElement | null) {}
 
+    eventsMap(): {[key : string]: () => void }{
+        return {
+            'click:button': this.onButtonClick,
+            'mouseenter:h1': this.onHeadHover
+        }
+    }
+
+    onButtonClick(){
+        console.log("Click!!");
+        
+    }
+
+    onHeadHover(){
+        console.log('hover');
+        
+    }
+
     template(): string{
         return `
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Click</button>
             </div>
         `;
+    }
+
+    bindEvents(fragment: DocumentFragment): void{
+        const eventsMap = this.eventsMap();
+
+        for(let eventKey in eventsMap){
+            const [eventName, selector] = eventKey.split(':');
+        
+            fragment.querySelectorAll(selector).forEach(element => { 
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            })
+        }
+
+
     }
 
     render(): void{
@@ -17,7 +49,13 @@ export class UserForm{
             const templateElement = document.createElement('template');
             templateElement.innerHTML = this.template();
 
+            this.bindEvents(templateElement.content);
+
             this.parent.append(templateElement.content);
         }
     }
+
+    
+
+
 }
